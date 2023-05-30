f864cc16e2d331f05ec9374eeb8c90ae75e7dc8c
kidneyweak <kidneyweak@gmail.com>
2023-05-30 22:47:10 +0800

    feat: deploy verifier and delete test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -119,9 +119,6 @@ const config: HardhatUserConfig = {
     ptau: 'https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau',
     circuits: [
       {
-        name: 'testV',
-      },
-      {
         name: 'slash',
       },
     ],
